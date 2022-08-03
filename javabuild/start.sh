f99ec51336f43246ab56ab25ab91ce8a9d501869
AndrewM <enigmatick@rambler.ru>
2022-08-03 21:37:57 +0300

    Correct params on build

--- javabuild/start.sh
+++ javabuild/start.sh
@@ -10,8 +10,8 @@ BTMP="/opt/tmp"
 BLOG="/opt/java/log/build.log"
 MVN="${MAVEN_HOME}/bin/mvn"
 
-[ -f "${BLOG}" ] || (echo "Cannot find ${BLOG}. Check if it was created!" && exit)
-git clone "${GIT_BUILD_LINK}" tmp/
-([ -d "${BTMP}" ] && cd "${BTMP}") || (echo "Cannot find ${BTMP}. Check GIT link!" && exit)
-([ -f "${MVN}" ] && mvn package -l "${BLOG}") || (echo "Build error! Check maven or pom file" && exit)
+[ -f ${BLOG} ] || (echo "Cannot find ${BLOG}. Check if it was created!" && exit)
+git clone "${GIT_BUILD_LINK}" ${BTMP}
+([ -d ${BTMP} ] && cd ${BTMP}) || (echo "Cannot find ${BTMP}. Check GIT link!" && exit)
+([ -f ${MVN} ] && mvn package -l ${BLOG}) || (echo "Build error! Check maven or pom file" && exit)
 exit 0
