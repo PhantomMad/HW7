4932aaa7de8addfcd4b0d5614c6fda8c5cf83d1e
AndrewBS <phantom@bstal.ru>
2022-08-04 14:39:43 +0300

    Work with prod path

--- start.sh
+++ start.sh
@@ -4,5 +4,7 @@ paxctl -c java
 paxctl -m java
 paxctl -c javac
 paxctl -m javac
+setfattr -n user.pax.flags -v "mr" java
+setfattr -n user.pax.flags -v "mr" javac
 cd /opt
 tomcat/bin/startup.sh run && tail -f tomcat/logs/catalina.out
