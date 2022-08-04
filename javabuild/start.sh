a2d1b8851b830edf648cfb9c37c0764682ef550c
AndrewBS <phantom@bstal.ru>
2022-08-04 11:01:16 +0300

    Correct build config stage

--- javabuild/start.sh
+++ javabuild/start.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-
-BTMP="/opt/tmp"
-BLOG="/opt/java/log/build.log"
-MVN="${MAVEN_HOME}/bin/mvn"
-
-[ -f ${BLOG} ] || (echo "Cannot find ${BLOG}. Check if it was created!" && exit)
-git clone "${GIT_BUILD_LINK}" ${BTMP}
-([ -d ${BTMP} ] && cd ${BTMP}) || (echo "Cannot find ${BTMP}. Check GIT link!" && exit)
-([ -f ${MVN} ] && exec mvn package -l ${BLOG}) || (echo "Build error! Check maven or pom file" && exit)
+mvn package -f /opt/tmp/ -l opt/webapps/build.log
